03e951ef8ddb0999716bac0bef2dbc077269f4ac
Rami Alrabi <romeoalrabi@hotmail.com>
2024-04-14 13:12:07 -0400

    Update frontend.js
    Smoothed the transition of project boxes for mobile

--- static/frontend.js
+++ static/frontend.js
@@ -5,15 +5,22 @@ function handleScroll() {
     projectBoxes.forEach(function(box) {
         var bounding = box.getBoundingClientRect();
         var viewportHeight = window.innerHeight || document.documentElement.clientHeight;
-        // Calculate the distance of the box from the top of the viewport
         var distanceFromTop = bounding.top - viewportHeight / 2;
-        // Calculate the scale factor based on the distance from the top
-        // Adjust the scaleFactor value to control the popping-out effect
-        var scaleFactor = Math.min(Math.max(1 - Math.abs(distanceFromTop) / (viewportHeight / 1), 0.5), 1);
-        // Adjust the maximum scale of the box
+        
+        // Calculate the scale factor based on the device width
+        var scaleFactor = 1;
+        if (window.innerWidth <= 600) { // Adjust threshold as needed
+            scaleFactor = Math.min(Math.max(1 - Math.abs(distanceFromTop) / (viewportHeight / 0.7), 0.5), 1);
+        } else {
+            scaleFactor = Math.min(Math.max(1 - Math.abs(distanceFromTop) / (viewportHeight / 1), 0.5), 1);
+        }
+        
         var maxScale = 0.9; // Adjust this value as needed
+        if (window.innerWidth <= 600) {
+            var maxScale = 0.85;
+        }
         scaleFactor = Math.min(scaleFactor, maxScale);
-        // Apply the scale to the box with smooth transition
+        
         box.style.transform = 'scale(' + scaleFactor + ')';
     });
 }
